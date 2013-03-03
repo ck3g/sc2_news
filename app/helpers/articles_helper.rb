f75083dc505ff354e3441387141cfc97985657c9
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2013-03-03 16:43:18 +0200

    replace article body's tags with icons using Replace Icons Strategy

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -14,4 +14,25 @@ module ArticlesHelper
   def article_hint_by_rule(rule)
     article_hint rule.first, I18n.t("hint.#{rule.first.first}_desc", icon: image_tag(rule.last))
   end
+
+  def cut(text)
+    text.split(Article::CUTTER).first
+  end
+
+  def replace_tags(text)
+    str = text.gsub Article::CUTTER, ""
+
+    ReplaceIconsStrategy.rules.each do |tags, icon|
+      tags.each do |tag_name|
+        str.gsub! "&lt;#{tag_name}&gt;", image_tag(icon)
+      end
+    end
+
+    str
+  end
+
+  def formatted(text, cut_text = false)
+    text = cut(text) if cut_text
+    replace_tags text
+  end
 end
