a764be37ce82fc024a5f61993e8923d75954b79e
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2013-03-24 14:30:14 +0200

    mail settings, translate emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ Sc2News::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: "starcraft.md" }
 
   # Enable threaded mode
   # config.threadsafe!
