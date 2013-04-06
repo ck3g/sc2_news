9398263c433d95b3dda29c57fad15bb733f05bc3
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2013-04-06 14:41:43 +0300

    change sock file for puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -76,7 +76,7 @@ end
 
 after 'deploy:stop', 'puma:stop'
 after 'deploy:start', 'puma:start'
-after 'deploy:restart', 'puma:restart'
+# after 'deploy:restart', 'puma:restart'
 
 _cset(:puma_cmd) { "#{fetch(:bundle_cmd, 'bundle')} exec puma" }
 _cset(:pumactl_cmd) { "#{fetch(:bundle_cmd, 'bundle')} exec pumactl" }
@@ -86,7 +86,8 @@ _cset(:puma_role) { :app }
 namespace :puma do
   desc 'Start puma'
   task :start, :roles => lambda { fetch(:puma_role) }, :on_no_matching_servers => :continue do
-    run "cd #{current_path} && #{fetch(:puma_cmd)} -t 4:4 -q -e #{puma_env} -b 'unix://#{shared_path}/sockets/puma.sock' -S #{fetch(:puma_state)} --control 'unix://#{shared_path}/sockets/pumactl.sock' >> #{shared_path}/log/puma-#{puma_env}.log 2>&1 &", :pty => false
+    run "rm #{shared_path}/sockets/*"
+    run "cd #{current_path} && #{fetch(:puma_cmd)} -t 4:4 -q -e #{puma_env} -b 'unix://#{shared_path}/sockets/unicorn.sock' -S #{fetch(:puma_state)} --control 'unix://#{shared_path}/sockets/pumactl.sock' >> #{shared_path}/log/puma-#{puma_env}.log 2>&1 &", :pty => false
   end
 
   desc 'Stop puma'
