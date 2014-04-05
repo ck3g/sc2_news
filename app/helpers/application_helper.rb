1633429c7b1757aaa6230b071910b4ef913f5ec2
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2014-04-05 10:16:50 +0300

    Fix icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,11 +5,7 @@ module ApplicationHelper
   end
 
   def flash_icon(type)
-    if type == :alert
-      content_tag 'i', '', class: 'icon-warning-sign'
-    else
-      content_tag 'i', '', class: 'icon-info-sign'
-    end
+    fa_icon type == :alert ? 'warning' : 'info'
   end
 
   def link_to_nav(title, path)
