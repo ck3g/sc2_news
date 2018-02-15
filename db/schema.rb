f5de7893247570965b4c71e2e28030e8a14007e7
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2018-02-15 20:40:11 +0100

    Add shadow ban options to profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130525145743) do
+ActiveRecord::Schema.define(version: 20180215193918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -150,8 +150,10 @@ ActiveRecord::Schema.define(version: 20130525145743) do
     t.string   "win_rate"
     t.string   "profile_url"
     t.datetime "synchronized_at"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.boolean  "shadow_ban_comments", default: false
+    t.boolean  "shadow_ban_chat",     default: false
   end
 
   add_index "profiles", ["country_id"], name: "index_profiles_on_country_id", using: :btree
